9d414173bed06c2c3c7fb210cac723b61c221e2e
Achmad Gabriel Glowdy <39531874+gabrielglowdy@users.noreply.github.com>
2023-03-29 21:14:01 +0700

    update data

---
+++ src/api/wedding-detail/routes/wedding-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * wedding-detail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::wedding-detail.wedding-detail');
